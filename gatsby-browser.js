901d70d5561f348e70df0ada3116eaa8093ac343
caiofurtado <caiohbfurtado@gmail.com>
2020-05-22 19:51:25 -0300

    created

---
+++ gatsby-browser.js
@@ -0,0 +1,8 @@
+//Variáveis ou bibliotecas de Frontend
+/**
+ * Implement Gatsby's Browser APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/browser-apis/
+ */
+
+// You can delete this file if you're not using it
---
+++ gatsby-node.js
@@ -0,0 +1,8 @@
+//todo o conjunto de dados do gatsby vai ser passado aqui... posts, etc...
+/**
+ * Implement Gatsby's Node APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/node-apis/
+ */
+
+// You can delete this file if you're not using it
